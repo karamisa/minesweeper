fdba71eb7837b5073c3f2060841f4301f7bf222d
karamisa <118884723+karamisa@users.noreply.github.com>
2022-11-23 17:44:38 +0200

    first upload

---
+++ js/game.js
@@ -0,0 +1,176 @@
+'use strict'
+const MINE = '💣'
+const MARK = '🚩'
+
+var gBoard = []
+
+
+var gLevel = {
+    SIZE: 10,
+    MINES: 4,
+}
+
+
+var gGame = {
+    isOn: false,
+    shownCount: 0, markedCount: 0, secsPassed: 0, lives: 3
+}
+
+function onInit() {
+    gBoard = buildBoard(gLevel.SIZE, gLevel.MINES)
+    renderBoard(gBoard, '.game-container')
+}
+
+//BUILDS BOARD WITH GIVEN SIZE
+function buildBoard(size) {
+    const board = []
+    for (var i = 0; i < size; i++) {
+        board.push([])
+        for (var j = 0; j < size; j++) {
+            board[i][j] = {
+                minesAroundCount: 0,
+                isShown: false,
+                isMine: false,
+                isMarked: false
+            }
+        }
+    }
+    return board
+}
+
+
+
+/*FUNCTION SETS MINES AND NEIGHBORING MINES 
+    - RANDOMLY SETS MINES ANYWHERE EXCLUDING THE FIRST CLICKED CELL LOCATION
+    - COUNTS AND SETS THE NEIBORING MINES COUNT FOR EACH CELL
+*/
+function setMinesNegsCount(board, excludedCoords) {
+
+    //RANDOMLY PLACING MINES EXCLUDING FIRST CELL CLICKED LOCATION
+    const possibleMineLocations = []
+    for (var i = 0; i < board.length; i++) {
+        for (var j = 0; j < board[0].length; j++) {
+            if (excludedCoords[0] !== i || excludedCoords[1] !== j) possibleMineLocations.push([i, j])
+        }
+    }
+    for (var i = 0; i < gLevel.MINES; i++) {
+        const randLocation = possibleMineLocations.splice(getRandomInt(0, possibleMineLocations.length), 1)[0]
+        board[randLocation[0]][randLocation[1]].isMine = true
+    }
+
+
+    //COUNTING AND SETTING NEIGHBORING MINE COUNT
+    for (var i = 0; i < board.length; i++) {
+        for (var j = 0; j < board.length; j++) {
+            const cell = board[i][j]
+            var minesNegsCount = 0
+            for (var w = -1; w <= 1; w++) {
+                for (var z = -1; z <= 1; z++) {
+                    const neighborCellCoords = [i + w, j + z]
+                    if (neighborCellCoords[0] >= 0 && neighborCellCoords[0] < board[0].length &&
+                        neighborCellCoords[1] >= 0 && neighborCellCoords[1] < board.length) {
+                        const neighborCell = board[i + w][j + z]
+                        if (neighborCell.isMine) minesNegsCount++
+                    }
+                }
+            }
+            cell.minesAroundCount = minesNegsCount
+        }
+    }
+
+}
+
+
+
+/*FUNCTION CALLED WHEN CELLS ARE CLICKED (LEFT) 
+    //IF FIRST CELL CLICKED BOARD IS MINES ARE SET AND NEIGHBORS ARE COUNTED AND SET
+    //IF CELL IS (!isMarked) & (!isShown) IT IS SENT TO SHOW CELL FUNCTION.
+*/
+function cellClicked(elCell, i, j) {
+    const cell = gBoard[i][j]
+    if (gGame.shownCount === 0) {
+        setMinesNegsCount(gBoard, [i, j])
+    }
+    if (!cell.isMarked && !cell.isShown) showCell(elCell, i, j)
+    checkGameOver()
+
+}
+
+
+
+/*FUNCTION CALLED WHEN CELLLS ARE LEFT CLICKED. 
+    - DISABLES DEFAULT CONTEXT MENU
+    - IF CELL IS NOT SHOWN IT WILL TOGGLE A MARK.
+*/
+function cellMarked(elCell, i, j, event) {
+    event.preventDefault()
+    const cell = gBoard[i][j]
+    if (!cell.isShown) {
+        if (!cell.isMarked) {
+            cell.isMarked = true
+            elCell.innerHTML = MARK
+            gGame.markedCount++
+        } else {
+            cell.isMarked = false
+            elCell.innerHTML = '?'
+            gGame.markedCount--
+        }
+    }
+    checkGameOver()
+}
+
+
+
+//REVEALS ANY UNSHOWN NEIGHBORS OF A CELL WITH ZERO NEIGHBORING MINES 
+function expandShown(board, i, j) {
+    const cell = board[i][j]
+    for (var w = -1; w <= 1; w++) {
+        for (var z = -1; z <= 1; z++) {
+            const neighborCellCoords = [i + w, j + z]
+            if (neighborCellCoords[0] >= 0 && neighborCellCoords[0] < board[0].length &&
+                neighborCellCoords[1] >= 0 && neighborCellCoords[1] < board.length) {
+                const neighborCell = board[i + w][j + z]
+                if (!neighborCell.isShown) {
+                    const elNeighborCell = document.querySelector(`.cell-${neighborCellCoords[0]}-${neighborCellCoords[1]}`)
+                    showCell(elNeighborCell, neighborCellCoords[0], neighborCellCoords[1])
+                }
+            }
+        }
+    }
+}
+
+
+
+/*FUNCTION THAT REVEALS CELL
+    IF CELL IS A MINE, A MINE IS REVEALED. 
+    IF CELL IS NOT A MINE IT REVEALS NEIGHBORING MINE COUNT. 
+    IF NEIGHBORING MINE COUNT IS ZERO IT CALLS expandShown()
+*/
+function showCell(elCell, i, j) {
+    const cell = gBoard[i][j]
+    cell.isShown = true
+    gGame.shownCount++
+    var cellContent = ''
+    if (cell.isMine) {
+        cellContent=MINE
+        gGame.lives--
+    } else {
+        cellContent=cell.minesAroundCount
+    }
+
+    elCell.innerHTML = cellContent
+    if (!cell.isMine && cell.minesAroundCount === 0) expandShown(gBoard, i, j)
+}
+
+
+
+//CHECKS IF THE GAME IS OVER 
+function checkGameOver() {
+    if (gGame.markedCount===gLevel.MINES && 
+        gGame.shownCount===(gLevel.SIZE**2)-gLevel.MINES){
+            console.log('winner!')
+        }
+}
+
+
+
